386f595c08915fdcc77ee5254cbf8a8184ffb7e1
lennyaiko <lennyaiko17@gmail.com>
2024-04-01 18:02:09 +0100

    work on resources and collections

---
+++ app/Http/Resources/V1/CustomerResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type' => $this->type,
+            'email' => $this->email,
+            'address' => $this->address,
+            'city' => $this->city,
+            'postalCode' => $this->postal_code,
+        ];
+    }
+}
