75e3f5c946414dc251f78d2eddee7c84e644fa51
lennyaiko <lennyaiko17@gmail.com>
2024-04-01 17:23:26 +0100

    seeders now working

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use \App\Models\Customer;
+
 class CustomerSeeder extends Seeder
 {
     /**
@@ -12,6 +14,9 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Customer::factory()->count(25)->hasInvoices(10)->create();
+        Customer::factory()->count(100)->hasInvoices(5)->create();
+        Customer::factory()->count(100)->hasInvoices(3)->create();
+        Customer::factory()->count(5)->create();
     }
 }
