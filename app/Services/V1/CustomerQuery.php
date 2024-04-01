a732bdff219d7e937c813c8349f9180c65f2dde1
lennyaiko <lennyaiko17@gmail.com>
2024-04-01 19:48:10 +0100

    filter works

---
+++ app/Services/V1/CustomerQuery.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\V1;
+
+use Illuminate\Http\Request;
+
+class CustomerQuery
+{
+
+    // params that can be queried
+    protected $safeParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt']
+    ];
+
+    // mapping params to actual db name
+    protected $columnMap = [
+        'postalCode' => 'postal_code'
+    ];
+
+    // transform the operators from query string to what eloquent will need
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+    ];
+
+    public function transform(Request $request)
+    {
+        $eloQuery = [];
+
+        foreach ($this->safeParams as $param => $operators) {
+            $query = $request->query($param);
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloQuery;
+    }
+}
